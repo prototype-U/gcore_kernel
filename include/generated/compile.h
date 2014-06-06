c5461c0d435b752918d224a6db3cf90bd5f4e996
hell-lock-xda <naman.arora11@gmail.com>
2014-06-06 10:22:19 -0700

    Dirty hacks to make otg work

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Wed Jun 4 20:36:42 PDT 2014"
+#define UTS_VERSION "#7 SMP PREEMPT Fri Jun 6 10:20:26 PDT 2014"
 #define LINUX_COMPILE_BY "hell-lock"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0"
+#define UTS_RELEASE "3.4.0-g76693ab-dirty"
