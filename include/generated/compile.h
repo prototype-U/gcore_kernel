338e3a16aea1ec0abd3790f55b9f2622f68d59a5
hell-lock-xda <naman.arora11@gmail.com>
2014-06-06 03:27:10 -0700

    sources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Wed Jun 4 20:36:42 PDT 2014"
+#define LINUX_COMPILE_BY "hell-lock"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
