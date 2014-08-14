b59e0bd98f744b8fbf89d6b683fce1b2a03e4174
Stanislaw Halik <sthalik@misaki.pl>
2014-08-14 19:19:07 +0200

    crpaola

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 7 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP PREEMPT Fri Jun 6 10:20:26 PDT 2014"
-#define LINUX_COMPILE_BY "hell-lock"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
